9a87bd29527bfa8f7b02b26a408d4f7501e5e28b
Ricardo Caballero <rcabamo@gmail.com>
2013-11-17 00:53:50 +0100

    CONFIG hamburguer controllers

--- FinalBattleHack/FinalBattleHack/classses/RURAppDelegate.h
+++ FinalBattleHack/FinalBattleHack/classses/RURAppDelegate.h
@@ -7,9 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MSDynamicsDrawerViewController/MSDynamicsDrawerViewController.h>
 
 @interface RURAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MSDynamicsDrawerViewController *dynamicsDrawerViewController;
 
 @end
--- FinalBattleHack/FinalBattleHack/classses/RURAppDelegate.m
+++ FinalBattleHack/FinalBattleHack/classses/RURAppDelegate.m
@@ -10,8 +10,11 @@
 
 #import "RURAssembly.h"
 
-#import <MSDynamicsDrawerViewController/MSDynamicsDrawerViewController.h>
+#import "RURShopsMapVC.h"
+#import "RURPurchasesVC.h"
+#import "RURProfileVC.h"
 
+#import <MSDynamicsDrawerViewController/MSDynamicsDrawerViewController.h>
 #import <Typhoon/Typhoon.h>
 
 @implementation RURAppDelegate
@@ -22,14 +25,30 @@
     [self setupApp];
     
     // Init VC's
-    MSDynamicsDrawerViewController *rootVC = [MSDynamicsDrawerViewController new];
+    RURProfileVC *profileVC = [[RURProfileVC alloc] init];
+    RURPurchasesVC *purchasesVC = [[RURPurchasesVC alloc] init];
+    RURShopsMapVC *shopsVC = [[RURShopsMapVC alloc] init];
+    
+    self.dynamicsDrawerViewController = [MSDynamicsDrawerViewController new];
+    [self.dynamicsDrawerViewController addStylersFromArray:@[[MSDynamicsDrawerScaleStyler styler], [MSDynamicsDrawerFadeStyler styler]] forDirection:MSDynamicsDrawerDirectionLeft];
+    [self.dynamicsDrawerViewController addStylersFromArray:@[[MSDynamicsDrawerParallaxStyler styler]] forDirection:MSDynamicsDrawerDirectionRight];
+    
+    // Set main controller
+    [self.dynamicsDrawerViewController setPaneViewController:[[UINavigationController alloc] initWithRootViewController:shopsVC] animated:NO completion:nil];
+    
+    // Set lateral controllers
+    [self.dynamicsDrawerViewController setDrawerViewController:purchasesVC forDirection:MSDynamicsDrawerDirectionLeft];
+    [self.dynamicsDrawerViewController setDrawerViewController:profileVC forDirection:MSDynamicsDrawerDirectionRight];
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.rootViewController = rootVC;
+    self.window.rootViewController = self.dynamicsDrawerViewController;
     [self.window makeKeyAndVisible];
     UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"windowBackground"]];
     [self.window addSubview:backgroundView];
     [self.window sendSubviewToBack:backgroundView];
+    
+    NSLog(@"clientID: %@", [[TyphoonComponentFactory defaultFactory] clientID]);
+    NSLog(@"secretID: %@", [[TyphoonComponentFactory defaultFactory] secretID]);
     
     return YES;
 }
