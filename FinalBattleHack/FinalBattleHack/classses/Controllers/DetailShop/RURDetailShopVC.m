650461526f2c4d1df197426cdddf34f4b3f54418
Oriol Capdevila <uri.kpd@gmail.com>
2013-11-17 15:31:07 +0100

    More UI stuff

--- FinalBattleHack/FinalBattleHack/classses/Controllers/DetailShop/RURDetailShopVC.m
+++ FinalBattleHack/FinalBattleHack/classses/Controllers/DetailShop/RURDetailShopVC.m
@@ -36,12 +36,13 @@
     if (self) {
         //self.shop = shop;
         RUR_Shop *shopaux = [[RUR_Shop alloc] init];
-        [shopaux setName:@"American Eagle"];
-        [shopaux setDesc:@"Opening times:\nMon-Fry: 10am - 5pm\nSat: 9am - 6pm\nSun: Closed"];
-        [shopaux setAddress:@"Address Test"];
-        [shopaux setLon:122.33];
-        [shopaux setLat:37.89];
-        if ([[shop objectForKey:@"name"] isEqualToString:@"Tienda 1"]) {
+        
+        if ([[shop objectForKey:@"name"] isEqualToString:@"Apple Store"]) {
+            [shopaux setName:@"Apple Store"];
+            [shopaux setDesc:@"Opening times:\nMon-Fry: 10am - 5pm\nSat: 9am - 6pm\nSun: Closed"];
+            [shopaux setAddress:@"Address Test"];
+            [shopaux setLon:122.33];
+            [shopaux setLat:37.89];
             NSArray *pr = [[NSArray alloc] initWithObjects:
                            @{@"name":@"Magic Mouse!", @"desc":@"It began with iPhone. Then came iPod touch. Then MacBook Pro. Intuitive, smart, dynamic. Multi-Touch technology introduced a remarkably better way to interact with your portable devices — all using gestures. Now we’ve reached another milestone by bringing gestures to the desktop with a mouse that’s unlike anything ever before. It's called Magic Mouse. It's the world's first Multi-Touch mouse",  @"image":@"gestures_2x.jpg", @"price":@"60.0"},
                            @{@"name":@"Magic Mug!", @"desc":@"Magic mug it's unique in it's design. When you drop hot liquid on it, the legs begin to run in circles of about 30cm of radius. Be carefull where you place it, even being magic it breaks",  @"image":@"WhiteMugFront.jpg", @"price":@"23.0"},
@@ -50,15 +51,19 @@
                            @{@"name":@"Magic Mouse! 3", @"desc":@"It began with iPhone. Then came iPod touch. Then MacBook Pro. Intuitive, smart, dynamic. Multi-Touch technology introduced a remarkably better way to interact with your portable devices — all using gestures. Now we’ve reached another milestone by bringing gestures to the desktop with a mouse that’s unlike anything ever before. It's called Magic Mouse. It's the world's first Multi-Touch mouse",  @"image":@"gestures_2x.jpg", @"price":@"60.0"},
                            @{@"name":@"Magic Mug! 3", @"desc":@"Magic mug it's unique in it's design. When you drop hot liquid on it, the legs begin to run in circles of about 30cm of radius. Be carefull where you place it, even being magic it breaks",  @"image":@"WhiteMugFront.jpg", @"price":@"23.0"}, nil];
             [shopaux setProducts:pr];
+            [self.bigImage setImage:[UIImage imageNamed:@"passeigdegracia.png"]];
         } else {
+            [shopaux setName:@"Zara"];
+            [shopaux setDesc:@"Opening times:\nMon-Fry: 10am - 5pm\nSat: 9am - 6pm\nSun: Closed"];
+            [shopaux setAddress:@"Address Test"];
+            [shopaux setLon:122.33];
+            [shopaux setLat:37.89];
             NSArray *pr = [[NSArray alloc] initWithObjects:
-                           @{@"name":@"Magic Mouse!", @"desc":@"It began with iPhone. Then came iPod touch. Then MacBook Pro. Intuitive, smart, dynamic. Multi-Touch technology introduced a remarkably better way to interact with your portable devices — all using gestures. Now we’ve reached another milestone by bringing gestures to the desktop with a mouse that’s unlike anything ever before. It's called Magic Mouse. It's the world's first Multi-Touch mouse",  @"image":@"gestures_2x.jpg", @"price":@"60.0"},
-                           @{@"name":@"Magic Mug!", @"desc":@"Magic mug it's unique in it's design. When you drop hot liquid on it, the legs begin to run in circles of about 30cm of radius. Be carefull where you place it, even being magic it breaks",  @"image":@"WhiteMugFront.jpg", @"price":@"23.0"},
-                           @{@"name":@"Magic Mouse! 2", @"desc":@"It began with iPhone. Then came iPod touch. Then MacBook Pro. Intuitive, smart, dynamic. Multi-Touch technology introduced a remarkably better way to interact with your portable devices — all using gestures. Now we’ve reached another milestone by bringing gestures to the desktop with a mouse that’s unlike anything ever before. It's called Magic Mouse. It's the world's first Multi-Touch mouse",  @"image":@"gestures_2x.jpg", @"price":@"60.0"},
-                           @{@"name":@"Magic Mug! 2", @"desc":@"Magic mug it's unique in it's design. When you drop hot liquid on it, the legs begin to run in circles of about 30cm of radius. Be carefull where you place it, even being magic it breaks",  @"image":@"WhiteMugFront.jpg", @"price":@"23.0"},
-                           @{@"name":@"Magic Mouse! 3", @"desc":@"It began with iPhone. Then came iPod touch. Then MacBook Pro. Intuitive, smart, dynamic. Multi-Touch technology introduced a remarkably better way to interact with your portable devices — all using gestures. Now we’ve reached another milestone by bringing gestures to the desktop with a mouse that’s unlike anything ever before. It's called Magic Mouse. It's the world's first Multi-Touch mouse",  @"image":@"gestures_2x.jpg", @"price":@"60.0"},
-                           @{@"name":@"Magic Mug! 3", @"desc":@"Magic mug it's unique in it's design. When you drop hot liquid on it, the legs begin to run in circles of about 30cm of radius. Be carefull where you place it, even being magic it breaks",  @"image":@"WhiteMugFront.jpg", @"price":@"23.0"}, nil];
+                           @{@"name":@"Structured Bowling", @"desc":@"Structured Bowling bag with pocket and studs",  @"image":@"Bolso1.png", @"price":@"80.0"},
+                           @{@"name":@"Mini Fur Shopper", @"desc":@"Furry leather high heel ankie bool",  @"image":@"Bolso2.png", @"price":@"59.0"},
+                           @{@"name":@"Wellies", @"desc":@"Mini messenger bag with clasp",  @"image":@"Bolso3.png", @"price":@"102.0"},nil];
             [shopaux setProducts:pr];
+            [self.bigImage setImage:[UIImage imageNamed:@"zara.png"]];
         }
         
         self.shop = shopaux;
@@ -94,6 +99,12 @@
     UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"windowBackground"]];
     [self.view addSubview:backgroundView];
     [self.view sendSubviewToBack:backgroundView];
+    if ([[self.shop name] isEqualToString:@"Apple Store"]) {
+        [self.bigImage setImage:[UIImage imageNamed:@"passeigdegracia.png"]];
+    } else {
+        
+        [self.bigImage setImage:[UIImage imageNamed:@"zara.png"]];
+    }
 }
 
 - (IBAction)openProductsVC:(id)sender
