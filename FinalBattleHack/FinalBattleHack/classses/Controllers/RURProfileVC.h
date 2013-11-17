a605b0e4ed872b883819aea649fd370b4baba543
Raimon Lapuente <raimon@tastebuds.fm>
2013-11-17 01:04:35 +0000

    Profile files

---
+++ FinalBattleHack/FinalBattleHack/classses/Controllers/RURProfileVC.h
@@ -0,0 +1,13 @@
+//
+//  RURProfileVC.h
+//  FinalBattleHack
+//
+//  Created by Ricardo Caballero on 17/11/13.
+//  Copyright (c) 2013 The Synthesizers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RURProfileVC : UIViewController
+
+@end
---
+++ FinalBattleHack/FinalBattleHack/classses/Controllers/RURProfileVC.m
@@ -0,0 +1,38 @@
+//
+//  RURProfileVC.m
+//  FinalBattleHack
+//
+//  Created by Ricardo Caballero on 17/11/13.
+//  Copyright (c) 2013 The Synthesizers. All rights reserved.
+//
+
+#import "RURProfileVC.h"
+
+@interface RURProfileVC ()
+
+@end
+
+@implementation RURProfileVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
